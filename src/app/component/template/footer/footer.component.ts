a22e8088f4d7eaafb760b5ccfb4c3fd634995d64
unknown <Administrator@SAO-341LN33.usersad.everis.int>
2023-03-01 15:18:54 -0300

    Configurado card da tela home

---
+++ src/app/component/template/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, Directive, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+
+export class FooterComponent {
+  
+}
+
+@Directive({
+  selector: '[appRed]',
+})
+export class FooterStyleRed {
+  constructor(el: ElementRef){
+    el.nativeElement.style.color = "#ff0000"
+  }
+}
+
